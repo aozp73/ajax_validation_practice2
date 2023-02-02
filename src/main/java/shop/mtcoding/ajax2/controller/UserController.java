a0071cb4ee50d07f813f3b018a548c5998c15fdb
aozp73 <aozp73@naver.com>
2023-02-02 19:23:30 +0900

    세팅 완료

---
+++ src/main/java/shop/mtcoding/ajax2/controller/UserController.java
@@ -0,0 +1,8 @@
+package shop.mtcoding.ajax2.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
---
+++ src/test/java/shop/mtcoding/ajax2/Ajax2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.ajax2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ajax2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
